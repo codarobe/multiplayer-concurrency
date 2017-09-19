6672017d815ea647be075fe35434ffa11ca11080
Cody Robertson <codarobe@ut.utm.edu>
2017-09-18 22:01:20 -0500

    Updated server/client code

--- New Unity Project 4/Assets/Scripts/networkController.cs
+++ New Unity Project 4/Assets/Scripts/networkController.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class networkController : MonoBehaviour {
 	public int maxConnections;
 	private int hostID;
 	private int connectionID;
 	private int myReliableChannelID;
 	private int myUnreliableChannelID;
+
+	private int[] connections;
 
 	public bool isHost;
 	public string ip;
@@ -18,7 +21,10 @@ public class networkController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		// Initializing the Transport Layer with no arguments (default settings)
+		Debug.Log("HERE");
 		NetworkTransport.Init();
+
+		connections = new int[maxConnections];
 
 		ConnectionConfig config = new ConnectionConfig();
 		// TCP Connection
@@ -26,83 +32,95 @@ public class networkController : MonoBehaviour {
 		// UDP Connection
 		myUnreliableChannelID = config.AddChannel(QosType.Unreliable);
 
-		if (isHost) {
-			HostTopology topology = new HostTopology (config, maxConnections);
 
-			// Initialized as host
-			hostID = NetworkTransport.AddHost (topology, 8888);
-		} else {
-			connectionID = NetworkTransport.Connect(hostID, "192.16.7.21", 8888, 0, out error);
+		HostTopology topology = new HostTopology (config, maxConnections);
 
-		}
+		// Initialized as host
+		hostID = NetworkTransport.AddHost (topology, port);
+
+		byte error;
+		connectionID = NetworkTransport.Connect(hostID, ip, port, 0, out error);
+		receiveData ();
+		//Debug.Log (error);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (isHost) {
+		sendData ();
+		for (int i = 0; i < connections.Length; i++) {
 			receiveData ();
-		} else {
-			sendData ();
 		}
 	}
 
 	void sendData() {
-		int recHostId; 
-		int connectionId; 
-		int channelId; 
+		int recHostID; 
+		int recConnectionID; 
+		int channelID; 
 		byte[] recBuffer = new byte[1024]; 
 		int bufferSize = 1024;
 		int dataSize;
 		byte error;
 
-		NetworkTransport.Send(hostID, connectionID, myReiliableChannelID, buffer, bufferLength,  out error);
+		NetworkTransport.Send(hostID, connectionID, myReliableChannelID, recBuffer, bufferSize,  out error);
 	}
 
 	void receiveData() {
-		int recHostId; 
-		int connectionId; 
-		int channelId; 
+		int recHostID; 
+		int recConnectionID; 
+		int channelID; 
 		byte[] recBuffer = new byte[1024]; 
 		int bufferSize = 1024;
 		int dataSize;
 		byte error;
 
 		// May throw a MessageToLong error
-		NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+
+		NetworkEventType recData = NetworkTransport.Receive(out recHostID, out recConnectionID, out channelID, recBuffer, bufferSize, out dataSize, out error);
+
+		//Debug.Log (error);
+
 		switch (recData)
 		{
 		case NetworkEventType.Nothing:         //1
 			//Debug.Log("nothing");
 			break;
 		case NetworkEventType.ConnectEvent:    //2
-			Debug.Log("Connection Found");
+			Debug.Log ("Connection Found");
 			//myConnectionID
-			if(hostID == connectionId)
+			if ((hostID == connectionID) && (connections.Length < maxConnections)) {
 				//my active connect request approved
-				Debug.Log("Successfully Connected");
-			else
+				Debug.Log ("Successfully Connected");
+				connections[connections.Length] = connectionID;
+			} else {
 				//somebody else connect to me
-				Debug.Log("Connection Rejected");
+				Debug.Log ("Connection Rejected");
+			}
 			break;
 		case NetworkEventType.DataEvent:       //3
 			Debug.Log("Data");
 			break;
 		case NetworkEventType.DisconnectEvent: //4
-			Debug.Log("Disconnection");
+			Debug.Log ("Disconnection");
 			//myConnectionID
-			if(hostID == connectionId)
+			if (hostID == connectionID) {
 				//cannot connect by some reason see error
-				Debug.Log("Error: Lost connection.");
-			else
+				Debug.Log ("Error: Lost connection.");
+			} else {
 				//one of the established connection has been disconnected
-				Debug.Log("Disconnected");
+				Debug.Log ("Disconnected");
+			}
 			break;
 		}
+		
+	
 	}
 
 	void onApplicationQuit() {
 		byte error;
 
 		NetworkTransport.Disconnect(hostID, connectionID, out error);
+
+		Debug.Log ("disconnected");
+		//Debug.Log (error);
 	}
 }
