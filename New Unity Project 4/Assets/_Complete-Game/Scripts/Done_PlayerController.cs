14ed637cd0283665a3d54e36d08c554d1bc1d1ff
Cody Robertson <codarobe@ut.utm.edu>
2017-10-24 10:51:22 -0500

    Changed camera movement

--- New Unity Project 4/Assets/_Complete-Game/Scripts/Done_PlayerController.cs
+++ New Unity Project 4/Assets/_Complete-Game/Scripts/Done_PlayerController.cs
@@ -75,8 +75,8 @@ public class Done_PlayerController : MonoBehaviour {
 			//Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 		//);
 
-		gameObject.transform.Rotate(0.0f, moveHorizontal * rotationSpeed, 0.0f);
-		Vector3 forwardVel = gameObject.transform.forward * speed * moveVertical;
+		GetComponent<Rigidbody>().transform.Rotate(0.0f, moveHorizontal * rotationSpeed, 0.0f);
+		Vector3 forwardVel = GetComponent<Rigidbody>().transform.forward * speed * moveVertical;
 		//Vector3 horizontalVel = transform.right * speed * moveHorizontal;
  
 		GetComponent<Rigidbody>().velocity = forwardVel;
